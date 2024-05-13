a7209db3826676546a67720d0e95c62401576f5b
Nityanand <n.n.r1204@gmail.com>
2024-05-13 22:41:36 +0530

    convert all the blogs of the list as a link

--- frontend/src/components/Blogs.tsx
+++ frontend/src/components/Blogs.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import useBlogs, { Blog } from "../hooks/useBlogs";
 import Bookmark from "../icons/Bookmark";
 import Plus from "../icons/Plus";
@@ -31,36 +32,40 @@ export default function Blogs() {
         {blogs?.length &&
           blogs.map((blog: Blog) => {
             return (
-              <div key={blog.id} className="py-10 flex justify-between gap-5 border-b-2 border-gray-100">
-                <div className="basis-/712">
-                  <div className="flex gap-2 mb-1  items-center">
-                    <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-400">
-                      <span className="text-xs text-gray-500 dark:text-gray-300">{getInitials(blog.author.name)}</span>
+              <Link to={`/blog/${blog.id}`}>
+                <div key={blog.id} className="py-10 flex justify-between gap-5 border-b-2 border-gray-100">
+                  <div className="basis-/712">
+                    <div className="flex gap-2 mb-1  items-center">
+                      <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-400">
+                        <span className="text-xs text-gray-500 dark:text-gray-300">
+                          {getInitials(blog.author.name)}
+                        </span>
+                      </div>
+                      <p>{blog.author.name}</p>
+                      <span className="text-xs text-gray-400">&#9679;</span>
+                      <p className="text-gray-400">Dec 3, 2023</p>
                     </div>
-                    <p>{blog.author.name}</p>
-                    <span className="text-xs text-gray-400">&#9679;</span>
-                    <p className="text-gray-400">Dec 3, 2023</p>
-                  </div>
-                  <div>
-                    <h2 className="font-bold text-2xl mb-2">{blog.title}</h2>
-                    <p className="mb-8">{blog.content}</p>
-                  </div>
-                  <div className="flex justify-between">
-                    <div className="flex items-center gap-4 text-gray-500">
-                      <span className="bg-gray-200 rounded-full py-1 px-2 text-gray-600 text-sm font-medium">
-                        side Hustle
-                      </span>
-                      <span>3 min read</span>
+                    <div>
+                      <h2 className="font-bold text-2xl mb-2">{blog.title}</h2>
+                      <p className="mb-8">{blog.content}</p>
                     </div>
-                    <div className="flex items-center gap-4">
-                      <Bookmark classes="w-5 h-5 text-gray-500" />
-                      <Remove classes="w-5 h-5 text-gray-500" />
-                      <span className="text-xl font-bold mb-3 inline-block text-gray-500">...</span>
+                    <div className="flex justify-between">
+                      <div className="flex items-center gap-4 text-gray-500">
+                        <span className="bg-gray-200 rounded-full py-1 px-2 text-gray-600 text-sm font-medium">
+                          side Hustle
+                        </span>
+                        <span>3 min read</span>
+                      </div>
+                      <div className="flex items-center gap-4">
+                        <Bookmark classes="w-5 h-5 text-gray-500" />
+                        <Remove classes="w-5 h-5 text-gray-500" />
+                        <span className="text-xl font-bold mb-3 inline-block text-gray-500">...</span>
+                      </div>
                     </div>
                   </div>
+                  <div className="basis-5/12 hidden md:block"></div>
                 </div>
-                <div className="basis-5/12 hidden md:block"></div>
-              </div>
+              </Link>
             );
           })}
       </section>
